e7ce072351e7499bceacfa40ed6bb437d91d8ad3
Vescik <46301368+Vescik@users.noreply.github.com>
2020-04-27 01:59:19 +0200

    info

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-alert('Pamiętaj, że to tylko wersja testowa, wygląd a tymbardziej funkcjonalność może się znacząco różnić w efekcie końcowym');
-alert('Strona może się źle wyświetlać na dużych ekranach, a na mobile nie jest jeszcze zrobiona i będzie dopiero na koniec');
-alert('Sekcja z dodawaniem postów jest dopiero zaczęta i nie działa dobrze, jeśli w ogóle');
+// alert('Pamiętaj, że to tylko wersja testowa, wygląd a tymbardziej funkcjonalność może się znacząco różnić w efekcie końcowym');
+// alert('Strona może się źle wyświetlać na dużych ekranach, a na mobile nie jest jeszcze zrobiona i będzie dopiero na koniec');
+// alert('Sekcja z dodawaniem postów jest dopiero zaczęta i nie działa dobrze, jeśli w ogóle');
+let close  = document.querySelector('.alert');
+function closeWindow() {
+    close.classList.add('active')
+}
