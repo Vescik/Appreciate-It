6d823aa2245cd4498b4db94da2e678b1881fed2a
Vescik <46301368+Vescik@users.noreply.github.com>
2020-04-27 00:25:45 +0200

    alert

--- js/app.js
+++ js/app.js
@@ -1,2 +1,3 @@
-// alert('Pamiętaj, że to tylko wersja testowa, wygląd a tymbardziej funkcjonalność może się znacząco różnić w efekcie końcowym');
-// alert('Jeśli strona nie wyświetla się poprawnie na twoim komputerze be patient work in progress');
+alert('Pamiętaj, że to tylko wersja testowa, wygląd a tymbardziej funkcjonalność może się znacząco różnić w efekcie końcowym');
+alert('Strona może się źle wyświetlać na dużych ekranach, a na mobile nie jest jeszcze zrobiona i będzie dopiero na koniec');
+alert('Sekcja z dodawaniem postów jest dopiero zaczęta i nie działa dobrze, jeśli w ogóle');
