de9cf209fa549c5cfa8a643b642a7cb93b348242
Vescik <46301368+Vescik@users.noreply.github.com>
2020-04-27 21:19:35 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@
 // alert('Strona może się źle wyświetlać na dużych ekranach, a na mobile nie jest jeszcze zrobiona i będzie dopiero na koniec');
 // alert('Sekcja z dodawaniem postów jest dopiero zaczęta i nie działa dobrze, jeśli w ogóle');
 let close  = document.querySelector('.alert');
+let textArea = document.querySelector('.addPost-input');
+const postHolder = [];
 function closeWindow() {
     close.classList.add('active')
+}
+
+function addPostToArray() {
+    postHolder.push(textArea.value);
+    console.log(postHolder)
 }
